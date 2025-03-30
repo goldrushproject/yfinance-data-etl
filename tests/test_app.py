436a72d38b3edcdee912cbf13b78d84a9354bbe3
Builderbot2000 <skenshin2000@outlook.com>
2025-03-30 00:23:07 -0700

    added more etl steps

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,18 +5,16 @@ from app.app import lambda_handler
 
 class TestApp(unittest.TestCase):
     def test_app(self):
-        event = {"max_time_window": 2, "ticker_symbol": "AAPL", "interval": "1m"}
+        event = {"max_time_window": 1, "ticker_symbol": "TSLA", "interval": "1m"}
         context = {}
         response = lambda_handler(event, context)
         try:
             self.assertEqual(response["statusCode"], 200)
-            self.assertIn("AAPL", response["body"])
-            self.assertIn("2", response["body"])
         except AssertionError as e:
             self.fail(f"Test failed: {e}")
         else:
             # Write response to a JSON file if the test succeeds
-            response_body = json.loads(response['body'])
+            response_body = response['body']
             with open('sample_data.json', 'w') as f:
                 json.dump(response_body, f, indent=4)
 
